aa24b9f528d68d4039ff68188ea44d41aa3d3c22
ImmortalBrick <34840787+Kanexis@users.noreply.github.com>
2020-02-28 15:53:44 +0800

    -_-

--- PlatformManagementConsole/Startup.cs
+++ PlatformManagementConsole/Startup.cs
@@ -72,7 +72,7 @@ namespace PlatformManagementConsole
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
 
-                
+               
             });
 
             app.UseSignalR(routes =>
